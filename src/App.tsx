0e9dea9c6d86edd827b60307fc690037a50ad34e
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-11-24 11:58:06 -0300

    feat: add Home, Footer and Navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { Title } from './components/Title/Title';
 import { Text  } from './components/Text/Text';
-import Navbar from './components/Navbar/Navbar';
+import { Navbar } from './components/Navbar/Navbar'
+import { Footer } from './components/Footer/Footer';
+import { Home } from './components/Home/Home';
 
 function App() {
   
@@ -11,6 +13,8 @@ function App() {
     <>
 
     <Navbar/>
+    <Home />
+    <Footer/>
       
       
 
