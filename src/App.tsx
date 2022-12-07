b89229ff44a9cd940de7dbae35909cf0085753a7
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-12-07 12:15:23 -0300

    feat: bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,16 @@ import {Login} from './Paginas/login/Login';
 import { CadastroUsuario } from './Paginas/cadastroUsuario/CadastroUsuario';
 import ListaTema from './components/temas/listaTema/listaTema';
 import ListaPostagem from './components/postagens/listaPostagem/listaPostagem';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Navbar />
+      <div style={{ minHeight: '100vh' }}>
       <Routes>
 
       <Route path="/" element={<Login  />} />
@@ -31,8 +35,10 @@ function App() {
 
 
       </Routes>
+      </div>
       <Footer />
     </Router>
+    </Provider>
 
   );
 }
--- src/components/estaticos/Navbar/Navbar.tsx
+++ src/components/estaticos/Navbar/Navbar.tsx
@@ -3,7 +3,26 @@ import { AppBar, Toolbar, Typography} from '@material-ui/core';
 import {Box} from '@mui/material';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import {useNavigate} from 'react-router-dom'
+import useLocalStorage from 'react-use-localstorage';
+import { Navigation } from '@mui/icons-material';
+import { addToken } from '../../../store/tokens/action';
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+
+
 function Navbar() {
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+    
+    function goLogout(){
+        dispatch(addToken(''))
+        alert("Usuario deslogado")
+        useNavigate.caller('/login')
+    }
     return (
         <>
             <AppBar position="static">
@@ -15,33 +34,41 @@ function Navbar() {
                     </Box>
 
                     <Box display="flex" justifyContent="start">
+                        <Link to="/home" className='text-decorator-none'>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 home
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to="/posts" className='text-decorator-none'>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 postagens
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to="/temas" className='text-decorator-none'>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 temas
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to="/formularioTema" className='text-decorator-none'>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 cadastrar tema
                             </Typography>
                         </Box>
-                        <Link to='/login' className='text-decorator-none'>
-                            <Box mx={1} style={{ cursor: "pointer", color: "white" }}>
+                        </Link>
+                        
+                            <Box mx={1} className='cursor' onClick={goLogout}>
                                 <Typography variant="h6" color="inherit">
                                     logout
                                 </Typography>
                             </Box>
-                        </Link>
+                        
                         
                     </Box>
 
